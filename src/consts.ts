dfe655e311e1651aa0ae5a8b0ec60ccd3f99e94b
khosbilegt <archerdoc13@gmail.com>
2026-04-03 11:54:26 +1100

    metadata fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
-export const SITE_TITLE = "Koso's Blog";
-export const SITE_DESCRIPTION = "You can find my thoughts and projects here.";
+export const SITE_TITLE =
+  "Full-Stack Developer & Freelancer | Koso Bilegsaikhan";
+export const SITE_DESCRIPTION =
+  "I'm a Full-Stack Engineer with over 6 years of professional experience based in Sydney, NSW. My red flag is reading a sci-fi, cautionary tale and saying: I kinda wanna make it. I know a little bit of everything, but know a bunch about software architecture, backend and DevOps. Followed up by web, mobile, AI, SaaS, Cloud, Infra etc. If you can name it, I probably know at least a little about it. If I'm quiet for any extended amount of time, that's probably me researching about it until I seem like I know a bit about it.";
